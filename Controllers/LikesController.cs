1378326a3a4782f4333a7a17464b5a5d1c41793d
Thabata <engthabata@gmail.com>
2021-05-16 22:53:33 -0300

    first commit

---
+++ Controllers/LikesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LikeButton.Models;
+using LikeButton.Models.Classes;
+
+namespace LikeButton.Controllers
+{
+    public class LikesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Likes
+        public ActionResult Index()
+        {
+            return View(db.Like.ToList());
+        }
+
+        // GET: Likes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Like like = db.Like.Find(id);
+            if (like == null)
+            {
+                return HttpNotFound();
+            }
+            return View(like);
+        }
+
+        // GET: Likes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Likes/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "article_id,article_like,like_date,like_user")] Like like)
+        {
+            if (ModelState.IsValid)
+            {
+                like.article_id = 0;
+                like.like_date = DateTime.Now;
+                db.Like.Add(like);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(like);
+        }
+
+        public ActionResult LikePost(int id)
+        {
+            Like update = db.Like.ToList().Find(u => u.article_id == id);
+            update.article_like += 1;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: Likes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Like like = db.Like.Find(id);
+            if (like == null)
+            {
+                return HttpNotFound();
+            }
+            return View(like);
+        }
+
+        // POST: Likes/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "article_id,article_like,like_date,like_user")] Like like)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(like).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(like);
+        }
+
+        // GET: Likes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Like like = db.Like.Find(id);
+            if (like == null)
+            {
+                return HttpNotFound();
+            }
+            return View(like);
+        }
+
+        // POST: Likes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Like like = db.Like.Find(id);
+            db.Like.Remove(like);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Classes/Like.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LikeButton.Models.Classes
+{
+    public class Like
+    {   
+        [Key]
+        public int  article_id { get; set; }
+        public int article_like { get; set; }
+        public DateTime like_date { get; set; }
+        public string like_user { get; set; } 
+
+    }
+}
